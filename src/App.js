bc9db44dd68386a393fd7a22e23f3b9f8b9f489e
Mario Mendez (LilB) <mariomendezmusic@gmail.com>
2019-06-16 14:53:25 -0700

    averaging in process, I think i need it

--- src/App.js
+++ src/App.js
@@ -5,9 +5,20 @@ import CounterDisplayButton from './CounterDisplayButton';
 function App() {
   const [ tapCount, setTapCount ] = useState(0);
   const [ elapsedTime, setElapsedTime ] = useState(0);
-  const [ bpm, setBPM ] = useState(120);
-  const [ msInterval, setMSInterval ] = useState(null);
+  const [ bpm, setBPM ] = useState(0);
+  const [ bpmArray, setBPMArray ] = useState([]);
+  const [ msInterval, setMSInterval ] = useState(1000);
+  const [ timerIsRunning, setTimerIsRunning ] = useState(false);
   let timer;
+
+  // space bar lets you tap
+  // 'r' is a reset
+  // light mode, dark mode, other modes
+  // if space bar is being used hide the tap button 
+  // useEffect(() => {
+  //   console.log(bpmArray);
+  //   setBPMArray(bpmArray.push(bpm))
+  // }, bpm);
 
   function useInterval(callback, delay) {
     const savedCallback = useRef();
@@ -29,14 +40,32 @@ function App() {
 
   useInterval(() => {
     setElapsedTime(elapsedTime + 1);
-  }, msInterval);
+  }, timerIsRunning ? msInterval : null);
+
+  function stopAndResetTimer() {
+    setTimerIsRunning(false);
+    setElapsedTime(0);
+  }
+
+
+  // BPM = tapCount * 60 sec / elapsedTime
+
+  function calculateBPM() {
+    setBPM((tapCount * 60 )/ elapsedTime);
+    setBPMArray(bpmArray.push(bpm))
+  }
+
+  function calculateBPMAverage() {
+
+  } 
 
   return (
     <div className="App">
-      {elapsedTime}
-      <CounterDisplayButton bpm={elapsedTime} />
-      {/* <button className="tap" startTimer={startTimer} ></button> */}
-      <button className="tap" onClick={() => setMSInterval(1000)} ></button>
+      <CounterDisplayButton bpm={bpm} />
+      elapsed timem {elapsedTime} and tap count {tapCount} bpm array is {bpmArray}
+      <button className="" onClick={() => setTimerIsRunning(!timerIsRunning)} >Start + Pause Button </button>
+      <button className="tap" onClick={() => {setTimerIsRunning(true); setTapCount(tapCount + 1); calculateBPM()}} >Tap Baby!</button>
+      <button onClick={() => stopAndResetTimer()} >Stop + Reset Button </button>
     </div>
   );
 }
--- src/CounterDisplayButton.js
+++ src/CounterDisplayButton.js
@@ -3,8 +3,34 @@ import React from 'react';
 const CounterDisplayButton = (props) => {
   const { bpm } = props;
   return (
-    <button>{bpm}</button>
+    // <button id="counter-display">{bpm === 0 ? "000" : bpm}</button>
+    <button id="counter-display">{printBPM(bpm)}</button>
   );
+}
+
+const printBPM = (bpm) => {
+  if (isNaN(bpm)) {
+    return "000";
+  }
+
+  const stringyBPM  = bpm.toFixed();
+
+  const leadingZero = "0";
+  // if (bpm > 99) {
+  //   return bpm
+  // } else if (bpm > 9) {
+  //   return leadingZero + bpm;
+  // } else {
+  //   return leadingZero + leadingZero + bpm;
+  // }
+
+  if (stringyBPM.length > 2) {
+    return stringyBPM;
+  } else if (stringyBPM.length > 1) {
+    return leadingZero + stringyBPM;
+  } else {
+    return leadingZero + leadingZero + bpm;
+  }
 }
 
 export default CounterDisplayButton;
